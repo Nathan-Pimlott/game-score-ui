7f0bdb04646a53eb8547b789cc330a6c1d77838e
Nathan Pimlott <nathanpimlott@hotmail.co.uk>
2025-01-10 12:26:04 +0000

    Cleaning tests

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -9,17 +9,4 @@ describe('Going to home page should render header bar', () => {
     cy.get(selectors.headerTitle).should('be.visible');
     cy.get(selectors.headerSubtitle).should('be.visible');
   });
-  // it('should add and subtract numbers correctly', () => {
-  //   cy.visit('/');
-
-  //   _.times(10, () => {
-  //     cy.get(selectors.subtractButton).click();
-  //   });
-  //   cy.get(selectors.currentCount).should('contain', 'Count is -10');
-
-  //   _.times(10, () => {
-  //     cy.get(selectors.addButton).click();
-  //   });
-  //   cy.get(selectors.currentCount).should('contain', 'Count is 0');
-  // });
 });
